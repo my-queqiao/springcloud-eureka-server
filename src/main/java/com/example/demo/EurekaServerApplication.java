734cbd7926b010bd166def2e93e3cccdd05ed415
pengju1234 <lizhao@huaching.com>
2019-04-14 16:00:34 +0800

    首次提交

---
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * eureka服务注册中心，提供服务的注册、发现
+ * @author tom
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
